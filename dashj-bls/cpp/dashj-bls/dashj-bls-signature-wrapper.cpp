8a31e767dd0c1b97b81797977e9427c6af4e0f1a
Hash Engineering Solutions <hashengineeringsolutions@gmail.com>
2021-11-18 21:40:21 -0800

    chore: update to version 0.18.0 (#3)

--- dashj-bls/cpp/dashj-bls/dashj-bls-signature-wrapper.cpp
+++ dashj-bls/cpp/dashj-bls/dashj-bls-signature-wrapper.cpp
@@ -688,7 +688,7 @@ SWIGEXPORT void JNICALL Java_org_dashj_bls_JNI_BLS_1SetContextError(JNIEnv *jenv
   core_get()->code = error;
 }
 
-std::string DASHJ_VERSION = "0.18-SNAPSHOT";
+std::string DASHJ_VERSION = "0.18.0";
 
 SWIGEXPORT jstring JNICALL Java_org_dashj_bls_JNI_BLS_1GetVersionString(JNIEnv *jenv, jclass jcls) {
   (void)jenv;
